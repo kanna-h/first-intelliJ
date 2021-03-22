0e91f64f923caa7dccba5767b602e9cfe625b745
KANNA <harikrishna010203@gmail.com>
2021-03-22 06:25:49 -0400

    counter changes for add

--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -9,10 +9,10 @@ public class CalculationTest {
     @Test
     public void testAdd() {
         System.out.println("========Junit test=========");
-        int a = 1; int b = 1;
-        when(calc.add(a, b)).thenReturn(2);
-        assertEquals(2, calc.add(a, b));
-        verify(calc).add(1, 1);
+        int a = 3; int b = 3;
+        when(calc.add(a, b)).thenReturn(6);
+        assertEquals(6, calc.add(a, b));
+        verify(calc).add(3, 3);
     }
 
 
